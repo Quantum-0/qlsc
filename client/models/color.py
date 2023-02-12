d020eef1586113d7f737a5c54a75cfc30c4a97f0
Anton Kurenkov <quantum0@mail.ru>
2023-02-12 17:32:45 +0300

    Update Color structure

--- client/models/color.py
+++ client/models/color.py
@@ -1,25 +1,31 @@
+from typing import Self
+
+
 class Color:
     """Color structure implementation"""
-    def __init__(self, r: int, g: int, b: int):
-        # TODO: raise ValueError
-        assert 0 <= r < 256
-        assert 0 <= g < 256
-        assert 0 <= b < 256
-        self.r = r
-        self.g = g
-        self.b = b
+    def __init__(self, red: int, green: int, blue: int) -> None:
+        if not 0 <= red < 256 or not 0 <= green < 256 or not 0 <= blue < 256:
+            raise ValueError('Invalid channels values for Color structure')
+        self.red = red
+        self.green = green
+        self.blue = blue
 
     @property
-    def R(self):
-        return self.r
+    def R(self) -> int:  # noqa pylint: disable=invalid-name
+        return self.red
 
     @property
-    def G(self):
-        return self.g
+    def G(self) -> int:  # noqa pylint: disable=invalid-name
+        return self.green
 
     @property
-    def B(self):
-        return self.b
+    def B(self)-> int:  # noqa pylint: disable=invalid-name
+        return self.blue
 
-    def __bytes__(self):
+    @classmethod
+    def from_HSV(cls, hue: int, saturation: int, value: int) -> Self:  # pylint: disable=invalid-name
+        # TODO
+        raise NotImplementedError()
+
+    def __bytes__(self) -> bytes:
         return self.R.to_bytes(1, 'big') + self.G.to_bytes(1, 'big') + self.B.to_bytes(1, 'big')
