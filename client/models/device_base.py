1ef26f9fd57d4213d0607e223cc767f453972b8e
Anton Kurenkov <quantum0@mail.ru>
2023-02-12 19:08:21 +0300

    Make device_base pydantic model instead of dataclass

--- client/models/device_base.py
+++ client/models/device_base.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pydantic
+from pydantic import Field
+
 if TYPE_CHECKING:
     from engine import QLPEngine
 
 # pylint: disable=wrong-import-position
 
-from dataclasses import dataclass
 from struct import pack
 
 from enums.commands import CommandID
@@ -15,16 +17,13 @@ from enums.packet_type import PacketType
 from models.packet import QLPPacket
 
 
-@dataclass
-class QLSCDeviceBase:
+class QLSCDeviceBase(pydantic.BaseModel):
     ip: str  # pylint: disable=invalid-name # TODO: type = ipaddress.IPv4Address
     device_chip_id: str
     device_uuid: str
     name: str
-    _engine: QLPEngine
-
-    def __post_init__(self):
-        self._length = 0
+    _engine: QLPEngine = Field(exclude=True)
+    _length: int = Field(default=0)
 
     def __hash__(self):
         return hash(self.device_chip_id)
