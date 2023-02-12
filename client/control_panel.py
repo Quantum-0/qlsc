f7c77ae7a9e1746fb22382a61f1a4c509b81395b
Anton Kurenkov <quantum0@mail.ru>
2023-02-12 19:08:56 +0300

    Update control panel, planning future api

--- client/control_panel.py
+++ client/control_panel.py
@@ -1,9 +1,9 @@
+import fastapi.exceptions
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from engine import QLPEngine
 from models.device import QLSCDevice
-from web.models.device import Device
 
 app = FastAPI()
 
@@ -17,8 +17,14 @@ async def live_check():
 async def search_for_devices():
     QLPEngine().start()
     devices = await QLPEngine().discover_all_devices()
-    devices.add(QLSCDevice('1.2.3.4', 'ABCDE', '12345', 'Test Device', QLPEngine()))
-    return [Device.from_dc(dev) for dev in devices]
+    devices.add(QLSCDevice(ip='1.2.3.4', device_chip_id='ABCDE', device_uuid='12345', name='Test Device'))
+    return devices
+
+@app.get('/api/devices/{device_uuid}')
+async def get_device_by_uuid(device_uuid):
+    if device_uuid == '12345':
+        return QLSCDevice(ip='1.2.3.4', device_chip_id='ABCDE', device_uuid='12345', name='Test Device')
+    raise fastapi.exceptions.HTTPException(404)
 
 
 app.mount("/", StaticFiles(directory="web/public", html=True))
