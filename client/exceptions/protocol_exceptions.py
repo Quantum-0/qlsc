e0027985495126286c29ed68ff5ae3ab1303cb7e
Anton Kurenkov <quantum0@mail.ru>
2023-02-12 17:44:05 +0300

    Add common responses code exception

--- client/exceptions/protocol_exceptions.py
+++ client/exceptions/protocol_exceptions.py
@@ -1,2 +1,31 @@
+from typing import Optional
+
+from enums.common_response_code import CommonResponseCode
+from models.device import QLSCDevice
+from models.packet import QLPPacket
+
+
 class QLPError(Exception):
     """Base Quantum0's LED Strip Protocol Exception"""
+
+
+class QLPResponseWithError(QLPError):
+    """Error from QLSCDevice"""
+    def __init__(
+            self,
+            device: QLSCDevice,
+            sent_packet: Optional[QLPPacket],
+            received_error: CommonResponseCode,
+            error_text: Optional[str]
+    ) -> None:
+        self.device = device
+        self.sent_packet = sent_packet
+        self.received_error = received_error
+        self.error_text = error_text
+        if received_error == CommonResponseCode.OTHER_ERROR and error_text is None:
+            raise QLPError('Got OTHER_ERROR but there is not description')
+        if received_error != CommonResponseCode.OTHER_ERROR and error_text is not None:
+            raise QLPError('Description is not available for any error exclude OTHER_ERROR')
+
+    def __repr__(self) -> str:
+        return f'<{self.__class__.__name__} {self.error_text or self.received_error.name}>'
